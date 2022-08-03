d67338ea8811801e99192b8e8eb7980b58f98459
dstark <gorokhov.dv93@gmail.com>
2022-08-03 23:38:22 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>Hello</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+div {
+  @include h1;
+  color: $green;
+}
+</style>
